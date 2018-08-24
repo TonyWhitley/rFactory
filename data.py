f463e424cd3f94c5bb18c0d7bbb1512db7513b80
TonyWhitley <Tony_Whitley@hotmail.com>
2018-08-24 16:00:49 +0100

    trawl_rF2_datafiles is smarter, extracts year and class from name tabCar gets data from data files.

--- data.py
+++ data.py
@@ -3,9 +3,9 @@ Accessing the data for rFactory.
 
 This reads rFactory's data files - one for each car, one for each track.
 File naming matches the name used by rFactor.  Data in the files is derived
-from rFactor but then enhanced - e.g. S397 cars and tracks do not specify that
+from rFactor but then enhanced - e.g. S397 __cars and __tracks do not specify that
 S397 is the author.  Additional data such as 
-cars:
+__cars:
 * type (open-wheeler, sports, GT, Indy, endurance, etc)
 * class (F1, GT3, GTE, etc)
 * type of gearshift (H, dogleg, paddles, sequential, auto, preselector!) 
@@ -13,7 +13,7 @@ cars:
 * year and decade of manufacture
 * star rating
 
-tracks:
+__tracks:
 * year and decade
 * continent
 * country
@@ -23,7 +23,11 @@ tracks:
 It also handles writing the files if they've been edited.
 """
 
-cars = {
+from rFactoryConfig import carTags,trackTags,CarDatafilesFolder, \
+  TrackDatafilesFolder,dataFilesExtension
+from trawl_rF2_datafiles import getListOfFiles, readFile, getTags
+
+__cars = {
   'dummyData' : [
           ['Ferrari',  '458', 'GT3', 'S397', 'GT', 'RWD', '2016', '2010-', '*****', 'car DB file'],
           ['Corvette', 'C7', 'GT3', 'S397', 'GT', 'RWD', '2016', '2010-', '*****', 'car DB file'],
@@ -37,5 +41,62 @@ cars = {
           ['Mazda',    '787B', 'Gp.C', 'MAK-Corp', 'GT', 'RWD', '1978', '1970-', '*****', 'MAK_Mazda_787B'],
           ['Ferrari',  '312', 'F1', 'Chief Wiggum/Postipate', 'Open-wheel', 'RWD', '1967', '1960-', '*****', 'car DB file'],
           ['Caterham', '7', 'C7', 'MikeeCZ', 'Sports', 'RWD', '2016', '2010-', '****', 'car DB file']
-        ]
+        ],
+  'tags' : []
   }
+
+__tracks = {
+  'dummyData' : [
+          ['Spa',  'S397', 'Historic', '1966', '1960-', '*****', 'track DB file']
+        ],
+  'tags' : []
+  }
+
+def __readDatafiles():
+  # Run this once to get the data
+  if len(__cars['tags']) == 0:
+    carFiles = getListOfFiles(path=CarDatafilesFolder, pattern='*'+dataFilesExtension)
+    trackFiles = getListOfFiles(path=TrackDatafilesFolder, pattern='*'+dataFilesExtension)
+
+    for car in carFiles:
+      text = readFile(car[0])
+      tags = getTags(text)
+      _entry = tags#{tags['Name'] : tags}
+      __cars['tags'].append(_entry)
+
+    for track in trackFiles:
+      text = readFile(track[0])
+      tags = getTags(text)
+      _entry = tags#{tags['Name'] : tags}
+      __tracks['tags'].append(_entry)
+  # else it's already loaded
+
+def getCarData(tags, maxWidth=30):
+  __readDatafiles()
+  _result = []
+  for _car in __cars['tags']:
+    _row = []
+    for tag in tags:
+      if tag == 'Manufacturer' and _car[tag] == '':
+        _row.append(_car['Name'][:maxWidth])
+      else:
+        _row.append(_car[tag][:maxWidth])
+    _result.append(_row)
+  return _result
+
+
+def getTrackData(tags, maxWidth=30):
+  __readDatafiles()
+  _result = []
+  for _car in __tracks['tags']:
+    _row = []
+    for tag in tags:
+      _row.append(_car[tag][maxWidth:])
+    _result.append(_row)
+  return _result
+
+
+if __name__ == '__main__':
+  # To run this by itself for development
+  __readDatafiles()
+  carData = getCarData(carTags)
