57c8e80bd2288e451aaee2e895ee3d1cad33cd46
TonyWhitley <Tony_Whitley@hotmail.com>
2018-08-28 16:26:55 +0100

    Basic data format changed from a list to a dict as it should have been all the time.

--- tabCar.py
+++ tabCar.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from tkinter import ttk
 
 from MC_table import Multicolumn_Listbox
-from rFactoryConfig import config_tabCar
+from rFactoryConfig import config_tabCar, CarDatafilesFolder
 from data import getAllCarData
 import carNtrackEditor
 
@@ -19,7 +19,6 @@ class Tab:
     """ Put this into the parent frame """
     self.parentFrame = parentFrame
     self.settings = None #PyLint
-    #tabCar.carColumns = ['Manufacturer', 'Model', 'Class', 'Author', 'Type', 'F/R/4WD', 'Year', 'Decade', 'Rating', 'Car DB file (hidden)']
     o_carData = self.__CarData()
     carData = o_carData.fetchData()
 
@@ -36,12 +35,13 @@ class Tab:
     colWidths = []
     for col in config_tabCar['carColumns']:
       colWidths.append(len(col))
-    for row in carData:
+    for __, row in carData.items():
       for col, column in enumerate(row):
-        if len(column) > colWidths[col]:
-          colWidths[col] = len(column)
+        if len(row[column]) > colWidths[col]:
+          colWidths[col] = len(row[column])
       for col, column in enumerate(row):
         self.mc.configure_column(col, width=colWidths[col]*7+6)
+    # Hide the final column (contains DB file ID):
     self.mc.configure_column(len(config_tabCar['carColumns'])-1, width=0, minwidth=0)
     # Justify the data in the first three columns
     self.mc.configure_column(0, anchor='e')
@@ -49,12 +49,9 @@ class Tab:
     self.mc.configure_column(2, anchor='w')
     self.mc.interior.grid(column=0, row=1, pady=2, columnspan=len(config_tabCar['carColumns']))
 
-    #tabCar.carFilters = ['Manufacturer', 'Model', 'Class', 'Author', 'Type', 'F/R/4WD', 'Year', 'Decade', 'Rating']
     o_filter = self.__Filter(parentFrame, config_tabCar['carColumns'], colWidths, o_carData, self.mc)
-    col = 0
     for _filter in config_tabCar['carFilters']:
-      o_filter.makeFilter(_filter, carData, col)
-      col += 1
+      o_filter.makeFilter(_filter, carData)
    
     o_filter.filterUpdate(None) # Initial dummy filter to load data into table
 
@@ -89,7 +86,7 @@ class Tab:
     top.title("Car editor")
 
     fields = config_tabCar['carColumns']
-    o_tab = carNtrackEditor.Editor(top, fields, data, command=self.answer)
+    o_tab = carNtrackEditor.Editor(top, fields, data, DatafilesFolder=CarDatafilesFolder, command=self.answer)
     """
     t = 'Editor data: '
     for w in data:
@@ -107,36 +104,33 @@ class Tab:
   class __CarData:
     """ Fetch and filter the car data """
     def __init__(self):
-      self.dummyData = [
-          ['Ferrari',  '458', 'GT3', 'S397', 'GT', 'RWD', '2016', '2010-', '*****', 'car DB file'],
-          ['Corvette', 'C7', 'GT3', 'S397', 'GT', 'RWD', '2016', '2010-', '*****', 'car DB file'],
-          ['Bentley',  'Continental', 'GT3', 'S397', 'GT', 'RWD', '2016', '2010-', '*****', 'car DB file'],
-          ['Eve',      'F1', 'F1', 'ISI', 'Open-wheel', 'RWD', '1967', '1960-', '*****', 'Historic Challenge_EVE_1968'],
-          ['Spark',    'F1', 'F1', 'ISI', 'Open-wheel', 'RWD', '1967', '1960-', '*****', 'Historic Challenge_spark_1968'],
-          ['Porsche',  '917K', 'Gp.C', 'Apex', 'GT', 'RWD', '1967', '1960-', '*****', 'FLAT12_917k_1971'],
-          ['Lola',     'T70', 'Gp.C', 'Crossply', 'GT', 'RWD', '1974', '1970-', '***', 'car DB file'],
-          ['Sauber',   'C11', 'Gp.C', 'MAK-Corp', 'GT', 'RWD', '1978', '1970-', '*****', 'MAK_Sauber_C11'],
-          ['Porsche',  '962C', 'Gp.C', 'MAK-Corp', 'GT', 'RWD', '1978', '1970-', '*****', 'MAK_Porsche_062C'],
-          ['Mazda',    '787B', 'Gp.C', 'MAK-Corp', 'GT', 'RWD', '1978', '1970-', '*****', 'MAK_Mazda_787B'],
-          ['Ferrari',  '312', 'F1', 'Chief Wiggum/Postipate', 'Open-wheel', 'RWD', '1967', '1960-', '*****', 'car DB file'],
-          ['Caterham', '7', 'C7', 'MikeeCZ', 'Sports', 'RWD', '2016', '2010-', '****', 'car DB file']
-        ]
       self.data = None  # Pylint
       self.filteredData = None
     def fetchData(self):
       """ Fetch the raw data from wherever """
       self.data = getAllCarData(tags=config_tabCar['carColumns'], maxWidth=20)
-      #self.dummyData.copy() # dummy data for now
-      print('DEBUG')
-      for row in self.dummyData:
-        print(len(row), row)
       return self.data
     def filterData(self, filters):
-      """ filters is a list of column number,text pairs """
-      _data = self.data.copy()
-      for _filter in filters:
-        _data = [elem for elem in _data if elem[_filter[0]] == _filter[1]() or _filter[1]() == NOFILTER]
-      self.filteredData = _data
+      """ 
+      Filter items of the data dict that match all of the filter combobox selections.
+      filters is a list of column name, comboBox text() function pairs """
+      _data = []
+      for _item, _values in self.data.items():
+        _data.append(_values.items())
+
+      self.filteredData = []
+      for __, _row in self.data.items():
+        _match = True
+        for _filter in filters:
+          if _row[_filter[0]] != _filter[1]() and _filter[1]() != NOFILTER:
+            _match = False
+            continue
+        if _match:
+          _r = []
+          for colName in config_tabCar['carColumns']:
+            _r.append(_row[colName])
+          self.filteredData.append(_r)
+
       return self.filteredData
     def setSelection(self, settings):
       """ Match settings to self.data, set table selection to that row """
@@ -152,26 +146,27 @@ class Tab:
       self.o_carData = o_carData
       self.mc = mc
       self.filters = []
-    def makeFilter(self, name, carData, col):
-      _column = self.columns.index(name)
-      tkFilterText = tk.LabelFrame(self.mainWindow, text=name)
-      tkFilterText.grid(column=col, row=0, pady=0)
+    def makeFilter(self, filterName, carData):
+      tkFilterText = tk.LabelFrame(self.mainWindow, text=filterName)
+      _col = self.columns.index(filterName)
+      tkFilterText.grid(column=_col, row=0, pady=0)
 
       s = set()
-      for item in carData:
-        s.add(item[_column])
+      for __, item in carData.items():
+        s.add(item[filterName])
       vals = [NOFILTER] + sorted(list(s))
       #modderFilter = tk.StringVar()
       tkComboFilter = ttk.Combobox(
           tkFilterText,
           #textvariable=modderFilter,
-          width=self.colWidths[_column],
-          height=len(vals))
+          #height=len(vals),
+          height=10,
+          width=self.colWidths[_col])
       tkComboFilter['values'] = vals
       tkComboFilter.grid(column=1, row=0, pady=5)
       tkComboFilter.current(0)
       tkComboFilter.bind("<<ComboboxSelected>>", self.filterUpdate)
-      self.filters.append([_column, tkComboFilter.get])
+      self.filters.append([filterName, tkComboFilter.get])
     
     def filterUpdate(self, event):
       """ Callback function when combobox changes """
