903e9d6920bf7f1f034676e56680f5ea1a467d22
TonyWhitley <Tony_Whitley@hotmail.com>
2018-08-31 15:44:36 +0100

    Cope with ScriptedJsonEditor not being present

--- tabJsonEditor.py
+++ tabJsonEditor.py
@@ -1,15 +1,35 @@
+import os
 import sys
 
-sys.path.append('../ScriptedJsonEditor/ScriptedJsonEditor')
-from GUI import Tab as _Tab, JobFrames
+# Python 3
+import tkinter as tk
+from tkinter import ttk
 
-class Tab(_Tab):
-  def __init__(self, parentFrame):
-    x = _Tab(parentFrame, cwd='../ScriptedJsonEditor/ScriptedJsonEditor')
+if os.path.exists('../ScriptedJsonEditor/ScriptedJsonEditor'):
+  sys.path.append('../ScriptedJsonEditor/ScriptedJsonEditor')
+  from GUI import Tab as _Tab, JobFrames
 
-    tkLabelframe_jobSettings = x.tkLabelframe_jobSettings
+  class Tab(_Tab):
+    def __init__(self, parentFrame):
+      x = _Tab(parentFrame, cwd='../ScriptedJsonEditor/ScriptedJsonEditor')
 
-    o_tab = JobFrames(tkLabelframe_jobSettings)
+      tkLabelframe_jobSettings = x.tkLabelframe_jobSettings
 
-    o_tab.set_checkbutton('G25_jobs', 'Monitor', 1)
-    assert o_tab.get_checkbutton('G25_jobs', 'Monitor') == 1
+      o_tab = JobFrames(tkLabelframe_jobSettings)
+
+      o_tab.set_checkbutton('G25_jobs', 'Monitor', 1)
+      assert o_tab.get_checkbutton('G25_jobs', 'Monitor') == 1
+else:
+  class Tab:
+    def __init__(self, parentFrame):
+      """ Put this into the parent frame """
+      tkLabelServer = tk.Label(parentFrame, 
+                                  text='ScriptedJsonEditor module not present')
+      tkLabelServer.grid(column=4, row=3)
+    def getSettings(self):
+      """ Return the settings for this tab """
+      return ['Server', 'password']
+
+    def setSettings(self, settings):
+      """ Set the settings for this tab """
+      pass
