7277227988f622e8599a3fefe6d2ea3343db0001
TonyWhitley <Tony_Whitley@hotmail.com>
2019-10-11 15:26:16 +0100

    WIP: more refactoring trawl_rf2_datafille

--- data/tests/test_trawl_rf2_datafiles.py
+++ data/tests/test_trawl_rf2_datafiles.py
@@ -25,14 +25,46 @@ class Test_trawl_rF2_datafiles(unittest.TestCase):
         _to = r"datafiles\cars\USF2000_2016.rfactory.txt"
         _result = cdf.make_datafile_name(_from)
         assert _result == _to, _result
-    def test_get_mas_info(self):
+    def test_list_files_in_mas_files(self):
+        cdf = CarDataFiles()
+        files = cdf.dir_files_in_mas_files(
+            r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Vehicles\SuperCharged_Miata_Level1\1.32")
+        assert files != {}
+
+    def test_read_track_mas_files(self):
+        tdf = TrackDataFiles()
+        tags = dict()
+        tags = tdf.read_mas_files(tags,
+                                 r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Locations\BATHURST_2016_V3\3.0")
+        assert tags['Scene Description'] == 'bathurst2016', tags
+        assert tags['Latitude'] == '-33.26', tags
+        assert tags['Longitude'] == '149.33', tags
+    def test_read_car_mas_files(self):
+        cdf = CarDataFiles()
+        tags = dict()
+        tags = cdf.read_mas_files(tags,
+            r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Vehicles\SuperCharged_Miata_Level1\1.32")
+        assert tags['Gearshift'] == 'H6', tags
+        assert tags['Mass'] == '1036', tags
+        assert tags['F/R/4WD'] == 'REAR', tags
+    def test_get_mas_info_track(self):
         scns, mas_tags = getMasInfo(
-            r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Locations\BATHURST_2016_V3\3.0" )
-        pass
-    def test_new_data(self):
+            r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Locations\BATHURST_2016_V3\3.0")
+        assert scns == ['bathurst12h2016', 'bathurst2016'], scns
+        assert mas_tags == {'Latitude': '-33.26', 'Longitude': '149.33'},mas_tags
+    def test_get_mas_info_car(self):
+        scns, mas_tags = getMasInfo(
+            r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Vehicles\SuperCharged_Miata_Level1\1.32")
+        print(scns, mas_tags)
+        print('Hello')
+    def test_new_data_car(self):
         cdf = CarDataFiles()
         _from = r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Vehicles\USF2000_2016\1.94\USF2000_2016.mft"
         cdf.new_data(_from)
+    def test_new_data_track(self):
+        tdf = TrackDataFiles()
+        _from = r"c:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Installed\Locations\BATHURST_2016_V3\3.0\BATHURST_2016_V3.mft"
+        tdf.new_data(_from)
         pass
 
 
